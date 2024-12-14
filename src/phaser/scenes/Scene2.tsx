56b8c1174a62faac844b4f0fe5cfe7ec4a5281d3
D-Atharv <ad298suncity@gmail.com>
2024-12-14 14:06:52 +0530

    feat: fixed ship depth

--- src/phaser/scenes/Scene2.tsx
+++ src/phaser/scenes/Scene2.tsx
@@ -10,11 +10,16 @@ export default class Scene2 extends BaseScene {
     }
 
     create() {
+
+        // Create floor tiles
+        this.createFloor();
+
         const { width, height } = this.scale; // Get dynamic game dimensions
 
         // Add the player ship in the center of the screen
         this.ship = this.add.image(width / 2, height / 2, "ship").setOrigin(0.5, 0.5);
         this.ship.setScale(3); // Scale the ship to make it larger
+        this.ship.setDepth(1); // Ensure ship is above the floor or we called the createFloor() first to make sure the floor is below the ship
 
         // Add a debug message to display
         this.add.text(20, 20, "playingGame", {
@@ -25,8 +30,6 @@ export default class Scene2 extends BaseScene {
         // Enable keyboard input for the ship's movement
         this.cursors = this.input.keyboard!.createCursorKeys();
 
-        // Create floor tiles
-        this.createFloor();
     }
 
     createFloor() {
@@ -48,7 +51,7 @@ export default class Scene2 extends BaseScene {
             .setAngle(45); // Rotate the floor by 45 degrees
 
         // Adjust tile scaling slightly to remove gaps
-        floor.setTileScale(0.61, 0.21); // Adjust this as needed
+        floor.setTileScale(0.81, 0.81); // Adjust this as needed
         // floor.setTileScale(0.51, 0.51); 
     }
 
